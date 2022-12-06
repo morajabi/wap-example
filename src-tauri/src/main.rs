6123af240bf3fbd893f6bc36c441599596e4014d
Mohamad <mo.rajbi@gmail.com>
2022-12-06 15:22:07 +0330

    add wap master

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use webrtc_audio_processing::{InitializationConfig, Processor};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,6 +12,11 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let config = InitializationConfig {
+        num_capture_channels: 0,
+        ..InitializationConfig::default()
+    };
+    let _p = Processor::new(&config);
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
